3705c9655c7c6d6d4bd319962e3cf8afd99aab0d
NakHyun Choi <9naknak8@gmail.com>
2022-11-30 13:54:53 +0900

    test 10

--- config/production.py
+++ config/production.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://dbmasteruser:7nw?m8a!CNWTHd)nwLdODo5eS~Q@ls-73552afb2c0c9f23f45aa5b86375a4b2bcb4e31f.cnpd0ghscnrt.ap-northeast-2.rds.amaz>/flask_pybo2:5432'.format(
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user=os.getenv('DB_USER'),
     pw=os.getenv('DB_PASSWORD'),
     url=os.getenv('DB_HOST'),
     db=os.getenv('DB_NAME'))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-# SECRET_KEY = '7nw?m8a!CNWTHd)nwL{dODo5eS~Q'
+SECRET_KEY = '7nw?m8a!CNWTHd)nwLdODo5eS~Q'
 
 dictConfig({
     'version': 1,
